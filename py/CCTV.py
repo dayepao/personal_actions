229b4414bbde7df6c4929f4506303fc7073c9736
GitHub Action <action@github.com>
2021-07-20 17:21:27 +0800

    Update_personal_actions

--- py/CCTV.py
+++ py/CCTV.py
@@ -2,14 +2,17 @@ import re
 import time
 
 from bs4 import BeautifulSoup
-from selenium.webdriver import Edge
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 # pip install -U requests[security]
 
-driver = Edge(executable_path='C:\\Users\\ll057\\Desktop\\edgedriver_win64\\msedgedriver.exe')
+options = webdriver.EdgeOptions()
+options.headless = True
+driver = webdriver.ChromiumEdge("C:\\Users\\ll057\\Desktop\\edgedriver_win64\\msedgedriver.exe", options=options)
 url = "https://search.cctv.com/search.php?qtext=%E5%8D%A7%E5%BA%95%20%E6%99%AE%E6%B3%95%E6%A0%8F%E7%9B%AE%E5%89%A7&type=video"
 resoult = {}
-res = driver.get(url)
+driver.get(url)
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
 items = soup.find_all(class_='jvedio')
@@ -19,7 +22,7 @@ for item in items:
             resoult[item.a["title"]] = re.findall(r"targetpage=(.+?)&point=", item.a['href'])
 for page in range(1, 4):
     time.sleep(10)
-    page = driver.find_element_by_partial_link_text('下一页')
+    page = driver.find_element(By.LINK_TEXT, '下一页')
     page.click()
     time.sleep(10)
     html = driver.page_source
