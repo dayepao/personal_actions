920d3ae778d8f0d0ea6489d6602a72166d05e5c6
GitHub Action <action@github.com>
2022-08-17 21:58:49 +0800

    Update_personal_actions
    Update add_watermark.py

--- py/watermark/add_watermark.py
+++ py/watermark/add_watermark.py
@@ -1,11 +1,14 @@
 import colorsys
+import io
 import os
 import sys
 
+import fitz
 from PIL import Image, ImageDraw, ImageFont
+from collections import Counter
 
 # pip install pillow
-# pip install borb
+# pip install pymupdf
 
 
 # 获取资源文件路径
@@ -62,6 +65,13 @@ def color_diff(color1: tuple, color2: tuple):
     return pow(((2 + r_mean / 256) * (R ** 2) + 4 * (G ** 2) + (2 + (255 - r_mean) / 256) * (B ** 2)), 0.5)
 
 
+# 获取字典中出现次数最多的键值
+def get_most_freq_value_in_dict(dic: dict):
+    x1 = dict(Counter(dic.values()))
+    dic1SortList = sorted(x1.items(), key=lambda x: x[1], reverse=True)
+    return dic1SortList[0][0]
+
+
 # 给图片添加文字水印
 def add_text_watermark_to_image(img_path: str, text: str, font_path: str = resource_path(os.path.join('fonts', 'SourceHanSansCN-Regular.otf')), font_size: int | str = "auto", color: str = "#696969", opacity: int = 30, angle: int = 20, with_stroke: bool = True):
     assert os.path.exists(img_path), "图片不存在"
@@ -77,6 +87,7 @@ def add_text_watermark_to_image(img_path: str, text: str, font_path: str = resou
     if font_size == "auto":
         temp_font_size = int(text_canvas_size / 12)
         temp_font = ImageFont.truetype(font_path, temp_font_size)
+        # 判断水印文字是否过长
         while draw.textbbox((0, 0), text, temp_font)[2] > text_canvas_size:
             temp_font_size = int(temp_font_size * 0.9)
             temp_font = ImageFont.truetype(font_path, temp_font_size)
@@ -88,7 +99,6 @@ def add_text_watermark_to_image(img_path: str, text: str, font_path: str = resou
     wm_text = "{}  ".format(text) * (int(text_canvas.size[0] / text_width) + 1)  # 将文字复制成画布宽度的倍数
     wm_text = "{}\n\n\n".format(wm_text) * (int(text_canvas.size[1] / text_height) + 1)  # 将文字复制成画布高度的倍数
     draw_kargs["text"] = wm_text
-
     if with_stroke:
         # 确定文字描边颜色
         image_dominant_color = get_image_dominant_color(img)  # 获取图片主要颜色
@@ -105,7 +115,7 @@ def add_text_watermark_to_image(img_path: str, text: str, font_path: str = resou
     text_canvas = text_canvas.rotate(angle)  # 旋转画布
     # 合并图片
     text_canvas = text_canvas.resize((text_canvas_size, text_canvas_size))  # 缩放画布
-    img.paste(text_canvas, (int((img.size[0] - text_canvas_size)/2), int((img.size[1] - text_canvas_size)/2)), text_canvas)
+    img.paste(text_canvas, (int((img.size[0] - text_canvas_size) / 2), int((img.size[1] - text_canvas_size) / 2)), text_canvas)
     # img.show()
     # 保存图片
     if os.path.splitext(img_path)[1] != ".png":
@@ -115,10 +125,129 @@ def add_text_watermark_to_image(img_path: str, text: str, font_path: str = resou
     img.close()
 
 
+# https://pymupdf.readthedocs.io/en/latest/page.html#Page.insert_image
+# 给PDF添加文字水印（初版）
+def add_text_watermark_to_pdf_old(pdf_path: str, text: str, font_path: str = resource_path(os.path.join('fonts', 'SourceHanSansCN-Regular.otf')), font_size: int | str = "auto", color: str = "#696969", opacity: int = 30, angle: int = 20, with_stroke: bool = True):
+    """
+    生成整页水印图片后合并
+    """
+    assert os.path.exists(pdf_path), "PDF不存在"
+    draw_kargs = {}  # 水印参数
+    draw_kargs["xy"] = (0, 0)
+    # 打开PDF
+    size_dict = {}
+    size_list = []
+    pdf = fitz.Document(pdf_path)
+    # 获取PDF页面尺寸
+    for page in pdf:
+        size_dict[page.number] = (int(page.rect[2]) + 1, int(page.rect[3]) + 1)
+    # 尺寸去重
+    for size in size_dict.values():
+        if size not in size_list:
+            size_list.append(size)
+    # 创建水印图片
+    watermark_dict = {}
+    for size in size_list:
+        # 创建画布
+        text_canvas_size = int(pow((size[0] ** 2 + size[1] ** 2), 0.5))  # 计算斜边长度
+        text_canvas = Image.new("RGBA", (text_canvas_size * 4, text_canvas_size * 4))  # 提高水印分辨率
+        draw = ImageDraw.Draw(text_canvas)
+        # 创建字体
+        if font_size == "auto":
+            temp_font_size = int(text_canvas_size / 4)
+            temp_font = ImageFont.truetype(font_path, temp_font_size)
+            # 判断水印文字是否过长
+            while draw.textbbox((0, 0), text, temp_font)[2] > 3 * text_canvas_size:
+                temp_font_size = int(temp_font_size * 0.9)
+                temp_font = ImageFont.truetype(font_path, temp_font_size)
+            font_size = temp_font_size
+        font = ImageFont.truetype(font_path, font_size)
+        draw_kargs["font"] = font
+        # 确定文字数量，准备要绘制的文字
+        text_width, text_height = draw.textbbox((0, 0), text, font)[2:4]
+        wm_text = "{}  ".format(text) * (int(text_canvas.size[0] / text_width) + 1)  # 将文字复制成画布宽度的倍数
+        wm_text = "{}\n\n\n".format(wm_text) * (int(text_canvas.size[1] / text_height) + 1)  # 将文字复制成画布高度的倍数
+        draw_kargs["text"] = wm_text
+        if with_stroke:
+            draw_kargs["stroke_fill"] = (0, 0, 0)
+            draw_kargs["stroke_width"] = 1
+        # 绘制文字水印
+        rgba_text_color = hex_to_rgba(color, opacity)  # 转换颜色
+        draw_kargs["fill"] = rgba_text_color
+        draw.multiline_text(**draw_kargs)
+        text_canvas = text_canvas.rotate(angle)  # 旋转画布
+        text_canvas = text_canvas.resize((text_canvas_size, text_canvas_size))  # 缩放画布
+        text_canvas_byte_array = io.BytesIO()
+        text_canvas.save(text_canvas_byte_array, format="PNG", quality=90)
+        watermark_dict[size] = (text_canvas_byte_array.getvalue(), text_canvas_size)
+    # 将水印添加至PDF
+    for page in pdf:
+        x0 = int((page.rect[2] - watermark_dict[size_dict[page.number]][1]) / 2)
+        y0 = int((page.rect[3] - watermark_dict[size_dict[page.number]][1]) / 2)
+        x1 = int(page.rect[2] - x0)
+        y1 = int(page.rect[3] - y0)
+        watermark_rect = fitz.Rect(x0, y0, x1, y1)
+        page.insert_image(watermark_rect, stream=watermark_dict[size_dict[page.number]][0])
+    # 保存PDF
+    pdf.save("{}_wm.pdf".format(os.path.splitext(pdf_path)[0]), garbage=3, deflate=True)
+
+
+# https://pymupdf.readthedocs.io/en/latest/page.html#Page.write_text
+# https://pymupdf.readthedocs.io/en/latest/shape.html#Shape.insert_textbox
+# https://pymupdf.readthedocs.io/en/latest/textwriter.html#textwriter
 # 给PDF添加文字水印
 def add_text_watermark_to_pdf(pdf_path: str, text: str, font_path: str = resource_path(os.path.join('fonts', 'SourceHanSansCN-Regular.otf')), font_size: int | str = "auto", color: str = "#696969", opacity: int = 30, angle: int = 20, with_stroke: bool = True):
-    pass
+    assert os.path.exists(pdf_path), "PDF不存在"
+    # 打开PDF
+    size_dict = {}
+    size_list = []
+    pdf = fitz.Document(pdf_path)
+    # 获取PDF页面尺寸
+    for page in pdf:
+        size_dict[page.number] = (int(page.rect[2]), int(page.rect[3]))
+    # 尺寸去重
+    for size in size_dict.values():
+        if size not in size_list:
+            size_list.append(size)
+    # 创建字体
+    font = fitz.Font(fontfile=font_path)
+    most_freq_size = get_most_freq_value_in_dict(size_dict)
+    hypotenuse_len = int(pow((most_freq_size[0] ** 2 + most_freq_size[1] ** 2), 0.5))  # 计算斜边长度
+    if font_size == "auto":
+        temp_font_size = most_freq_size[1] / 12
+        while font.text_length(text, fontsize=temp_font_size) > 0.6 * hypotenuse_len:
+            temp_font_size = int(temp_font_size * 0.9)
+        font_size = temp_font_size
+    font_length = font.text_length(text, fontsize=font_size)  # 获取文字长度
+    # 创建不同尺寸的水印TextWriter
+    watermark_dict = {}
+    for size in size_list:
+        wm_rect = fitz.Rect(0, 0, size[0], size[1])
+        tw = fitz.TextWriter(wm_rect, opacity=opacity / 100, color=tuple(x/255 for x in hex_to_rgba(color, opacity))[0:3])
+        h = 0
+        text_rect = fitz.Rect(0, 0, font_length, 1.5 * font_size)
+        # 根据文字高度和长度填充水印TextWriter
+        while h < size[1]:
+            text_rect.x0 = 0
+            w = 0
+            while w < size[0]:
+                tw.fill_textbox(text_rect, text, font=font, fontsize=font_size)
+                text_rect += fitz.Rect(1.1 * font_length, 0, 1.1 * font_length + 100, 0)
+                w += font_length
+            text_rect += fitz.Rect(0, 4 * font_size, 0, 4 * font_size)
+            h += (font_size * 1.5)
+        # tw.fill_textbox(text_rect, text, font=font, fontsize=font_size)
+        watermark_dict[size] = tw
+    # 将水印添加至PDF
+    for page in pdf:
+        rotate_morph = (fitz.Point(int(page.rect[2] / 2), int(page.rect[3] / 2)), fitz.Matrix(1, 0, 0, 1, 0, 0).prerotate(angle))
+        watermark_dict[size_dict[page.number]].write_text(page, morph=rotate_morph)
+        # page.write_text(writers=watermark_dict[size_dict[page.number]])
+    # 保存PDF
+    pdf.save("{}_wm.pdf".format(os.path.splitext(pdf_path)[0]), garbage=3, deflate=True)
 
 
 if __name__ == "__main__":
-    add_text_watermark_to_image(resource_path("test.jpg"), '测试水印abcdefghijklmnopqrstuvwxyz', color="#ff0000")
+    # add_text_watermark_to_image(resource_path("test.jpg"), '测试水印', color="#ff0000")
+    # add_text_watermark_to_pdf(resource_path("test.pdf"), '测试水印abcdefghijklmnopqrstuvwxyz')
+    add_text_watermark_to_pdf(resource_path("test.pdf"), '测试水印abcdefghijklmnopqrstuvwxyz')
