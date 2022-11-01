5791b9683bcb67bf6db785af989e9847efd360a0
GitHub Action <action@github.com>
2022-11-01 23:47:35 +0800

    Update_personal_actions
    Create rename_replace.py

---
+++ py/rename_replace.py
@@ -0,0 +1,46 @@
+import copy
+import os
+
+cur_paths = ["Z:\\"]
+replace_list = [("：", " - "), (":", " - "), ("  ", " ")]
+
+
+def rename_and_return_child_dir(path, replace_list):
+    child_dir = []
+    for child_name in os.listdir(path):
+        child_path = os.path.join(path, child_name)
+        if os.path.isfile(child_path):
+            # 重命名操作
+            temp_name = child_name
+            for replace_tuple in replace_list:
+                temp_name = temp_name.replace(replace_tuple[0], replace_tuple[1])
+            new_name = temp_name
+            # if new_name != child_name and (not os.path.exists(child_path.replace("Z:\\\\server", r"C:\\Users\ll057\OneDrive - lyhscu\VPS"))):
+            if new_name != child_name:
+                print((child_path, os.path.join(path, new_name)))
+                os.rename(child_path, os.path.join(path, new_name))
+        if os.path.isdir(child_path):
+            # 重命名操作
+            temp_name = child_name
+            for replace_tuple in replace_list:
+                temp_name = temp_name.replace(replace_tuple[0], replace_tuple[1])
+            new_name = temp_name
+            if new_name != child_name:
+                print((child_path, os.path.join(path, new_name)))
+                os.rename(child_path, os.path.join(path, new_name))
+                child_dir.append(os.path.join(path, new_name))
+            else:
+                child_dir.append(child_path)
+    return child_dir
+
+
+while True:
+    temp_dir = []
+    for cur_path in cur_paths:
+        cur_child_dir = rename_and_return_child_dir(cur_path, replace_list)
+        if len(cur_child_dir) == 0:
+            continue
+        temp_dir.extend(cur_child_dir)
+    cur_paths = copy.deepcopy(temp_dir)
+    if cur_paths == []:
+        break
