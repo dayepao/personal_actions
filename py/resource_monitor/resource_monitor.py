02b23b4d302d1bccb89265424c77230adc97c3be
GitHub Action <action@github.com>
2022-11-20 00:02:49 +0800

    Update_personal_actions
    Update resource_monitor

--- py/resource_monitor/resource_monitor.py
+++ py/resource_monitor/resource_monitor.py
@@ -1,8 +1,24 @@
 import sys
 
-from PySide6.QtCore import Qt
+from PySide6.QtCore import Qt, QTimer
 from PySide6.QtGui import QGuiApplication, QKeyEvent
 from PySide6.QtWidgets import QApplication, QMainWindow, QTextEdit
+from sub_thread import resource_monitor
+
+# pip install PySide6
+# pip install psutil
+
+# apt install task-gnome-desktop
+
+# vi /etc/gdm3/daemon.conf
+# 在 security 下面添加 AllowRoot=true
+# 保存后退出vi
+# vi /etc/pam.d/gdm-password /etc/pam.d/gdm3-autologin
+
+# 注释掉这一行 auth required pam_succeed_if.so user != root quiet_success
+# #auth required pam_succeed_if.so user != root quiet_success
+# 保存后退出vi
+
 
 # apt install libegl1-mesa
 # apt install libqt5gui5
@@ -15,17 +31,55 @@ class mainwindow(QMainWindow):
         self.setWindowTitle("resource_monitor by @dayepao")
         try:
             self.screen = QGuiApplication.screens()[screen_num]
-        except IndexError:
+        except IndexError as e:
+            print(e)
             self.screen = QGuiApplication.primaryScreen()
         self.setGeometry(self.screen.geometry())  # 设置窗口大小为屏幕大小
         self.TextEdit = QTextEdit(self)
-        self.TextEdit.setGeometry(0, 0, self.geometry().width(), self.geometry().height())
+        self.TextEdit.setGeometry(0, 0, self.geometry().width()/3, self.geometry().height()/3)
         self.TextEdit.setReadOnly(True)
+        self.run()
+
+    def run(self):
+        self.resource_monitor_thread = resource_monitor()
+
+        def on_thread_finished():
+            self.TextEdit.setText("监控主线程已停止，正在尝试重启...")
+            timer = QTimer()
+            timer.singleShot(3000, self.resource_monitor_thread.start)
+        self.resource_monitor_thread.finished.connect(on_thread_finished)
+
+        def update_ui(resource: dict):
+            self.TextEdit.setText(self.get_system_utilization_text(resource))
+        self.resource_monitor_thread.signal.connect(update_ui)
+
+        self.resource_monitor_thread.start()
+
+    def stop(self):
+        if 'resource_monitor_thread' in dir(self):
+            self.resource_monitor_thread.quit()
+            self.resource_monitor_thread.wait()
+        self.close()
+
+    def get_system_utilization_text(self, resource: dict):
+        if type(resource["system_utilization"]) == str:
+            system_utilization_text = resource["system_utilization"]
+        else:
+            system_utilization_text = ""
+            system_utilization_text += "cpu_utilization: {}%\n".format(resource["system_utilization"]['cpu_utilization'])
+            system_utilization_text += "per_cpu_utilization: {}\n".format(resource["system_utilization"]['per_cpu_utilization'])
+            system_utilization_text += "cpu_freq: {}\n".format(resource["system_utilization"]['cpu_freq'])
+            system_utilization_text += "per_cpu_freq: {}\n".format(resource["system_utilization"]['per_cpu_freq'])
+            system_utilization_text += "memory_utilization: {}%\n".format(resource["system_utilization"]['memory_utilization'])
+        return system_utilization_text
 
     def keyPressEvent(self, event: QKeyEvent):
         if event.modifiers() == Qt.KeyboardModifier.ControlModifier:
             if event.key() == Qt.Key.Key_Q:
-                self.close()
+                self.stop()
+            if event.key() == Qt.Key.Key_L:
+                self.resource_monitor_thread.get_system_utilization_thread.quit()
+                # self.resource_monitor_thread.quit()
 
 
 if __name__ == '__main__':
---
+++ py/resource_monitor/sub_thread.py
@@ -0,0 +1,82 @@
+import psutil
+from PySide6.QtCore import QThread, QTimer, Signal
+
+# https://psutil.readthedocs.io/en/latest/
+
+
+class get_system_utilization(QThread):
+    signal = Signal(dict)
+
+    def __init__(self, parent=None) -> None:
+        super().__init__(parent=parent)
+
+    def get_system_utilization(self):
+        system_utilization = {}
+        system_utilization["cpu_utilization"] = psutil.cpu_percent()
+        system_utilization["per_cpu_utilization"] = psutil.cpu_percent(percpu=True)
+        system_utilization["cpu_freq"] = psutil.cpu_freq()  # MHz
+        system_utilization["per_cpu_freq"] = psutil.cpu_freq(percpu=True)  # MHz
+        system_utilization["memory_utilization"] = psutil.virtual_memory().percent
+        self.signal.emit(system_utilization)
+
+    def run(self):
+        timer = QTimer()
+        timer.timeout.connect(self.get_system_utilization)
+        timer.start(500)
+        self.exec()
+
+
+class resource_monitor(QThread):
+    signal = Signal(dict)
+
+    def __init__(self, parent=None) -> None:
+        super().__init__(parent=parent)
+
+    def emit_signal(self):
+        self.signal.emit(self.resource)
+
+    def handle_sub_thread(self, command: str, sub_thread: str):
+        if command == "stop":
+            if sub_thread in dir(self):
+                getattr(self, sub_thread).quit()
+                getattr(self, sub_thread).wait()
+        elif command == "restart":
+            if sub_thread in dir(self):
+                getattr(self, sub_thread).quit()
+                getattr(self, sub_thread).wait()
+                getattr(self, sub_thread).start()
+            else:
+                getattr(self, f"start_{sub_thread}")()
+
+    def start_get_system_utilization_thread(self):
+        self.get_system_utilization_thread = get_system_utilization()
+
+        def on_thread_finished():
+            self.resource["system_utilization"] = "系统使用率子线程已停止，正在尝试重启..."
+            timer = QTimer()
+            timer.singleShot(3000, lambda: self.handle_sub_thread("restart", "get_system_utilization_thread"))
+        self.get_system_utilization_thread.finished.connect(on_thread_finished)
+
+        def update_system_utilization(system_utilization: dict):
+            self.resource["system_utilization"] = system_utilization
+        self.get_system_utilization_thread.signal.connect(update_system_utilization)
+
+        self.get_system_utilization_thread.start()
+
+    def stop(self):
+        if 'emit_timer' in dir(self):
+            self.emit_timer.stop()
+        self.handle_sub_thread("stop", "get_system_utilization_thread")
+
+    def run(self):
+        self.resource = {"system_utilization": "正在获取系统资源使用情况..."}
+        self.emit_signal()
+
+        # 启动监控子线程
+        self.start_get_system_utilization_thread()
+
+        self.emit_timer = QTimer()
+        self.emit_timer.timeout.connect(self.emit_signal)
+        self.emit_timer.start(1000)
+        self.exec()
+        self.stop()
