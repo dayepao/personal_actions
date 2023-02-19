76c4f3cda63f91d4331a1dfa45ea4c7bee9132da
GitHub Action <action@github.com>
2023-02-19 13:55:20 +0800

    Update_personal_actions
    Update photo_manager.py

--- py/photo_manager/photo_manager.py
+++ py/photo_manager/photo_manager.py
@@ -10,19 +10,18 @@ if __name__ == "__main__":
     #     if file.lower().endswith("png"):
     #         convert_to_jpg(os.path.join(path, file))
 
-
-    # for result in check_photos.check_photos(path, check_photos.GPS_exif_is_empty):
-    #     print(result)
-
     # for result in check_photos.check_photos(path, check_photos.date_time_in_exif_is_empty):
     #     print(result)
         # set_date_time_in_Exif_exif(result)
 
-    for root, _, files in os.walk(path):
-        for file in files:
-            filepath = os.path.join(root, file)
-            if file.lower().endswith((".jpg", ".jpeg")):
-                fix_date_time_in_exif(filepath)
+    # for result in check_photos.check_photos(path, check_photos.GPS_exif_is_empty):
+    #     print(result)
+
+    # for root, _, files in os.walk(path):
+    #     for file in files:
+    #         filepath = os.path.join(root, file)
+    #         if file.lower().endswith((".jpg", ".jpeg")):
+    #             fix_date_time_in_exif(filepath)
 
     # for result in check_photos.check_photos(path, check_photos.date_time_in_filename_and_exif_is_different):
     #     print(result)
