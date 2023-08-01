5f2ce0823f6b044bd49a58cb98e7e8ac2c7b8e50
GitHub Action <action@github.com>
2023-08-01 22:44:34 +0800

    Update_personal_actions
    Update frp_keepalive.py

--- py/frp_keepalive.py
+++ py/frp_keepalive.py
@@ -4,11 +4,19 @@ new Env('frp 保活');
 """
 
 import os
+import re
+import sys
 
 import httpx
 
 # 从环境变量读取url列表
-url_list = os.environ.get("FRP_URL_LIST").split(",")
+url_list = os.environ.get("FRP_URL_LIST")
+
+if not url_list:
+    print("未发现有效的 FRP_URL_LIST 环境变量,退出程序!")
+    sys.exit()
+
+url_list = re.split("[;,]", url_list)
 
 for url in url_list:
     try:
