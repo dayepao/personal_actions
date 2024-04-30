ea718a9b98a56efcb8d37919249da5d99aeb8159
GitHub Action <action@github.com>
2024-04-30 11:50:27 +0800

    Update_personal_actions
    Update

--- py/checkupdate.py
+++ py/checkupdate.py
@@ -4,10 +4,10 @@ import os
 from bs4 import BeautifulSoup
 
 from utils_dayepao import http_request
+from notify_dayepao import send_message
 
 GH_ACTOR = os.environ.get('GH_ACTOR')
-PUSH_KEY = os.environ.get('PUSH_KEY')
-REPOS = ["AutoApiS", "uptime-status"]  # 仓库名称
+REPOS = ["uptime-status",]  # 仓库名称
 
 
 if __name__ == '__main__':
@@ -25,21 +25,6 @@ if __name__ == '__main__':
                 pushstr = pushstr + repo + "有更新" + "\n\n"
     if pushstr:
         pushstr = now + "\n\n" + pushstr
-        print(pushstr)
-        if PUSH_KEY is not None:
-            pushurl = "https://push.dayepao.com/?pushkey=" + PUSH_KEY
-            pushdata = {
-                "touser": "@all",
-                "msgtype": "text",
-                "agentid": 1000002,
-                "text": {
-                    "content": pushstr
-                },
-                "safe": 0,
-                "enable_id_trans": 0,
-                "enable_duplicate_check": 0,
-                "duplicate_check_interval": 0
-            }
-            http_request("post", pushurl, postjson=pushdata, timeout=10)
+        send_message("GitHub 仓库更新提醒", pushstr)
     else:
         print(now + "\n\n" + "所有仓库都是最新的")
