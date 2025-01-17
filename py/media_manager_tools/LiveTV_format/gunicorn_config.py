2f909e459a8f7941c6dafa7cef38d9b0605bd753
GitHub Action <action@github.com>
2025-01-17 20:05:12 +0800

    Update_personal_actions
    Update gunicorn_config.py

--- py/media_manager_tools/LiveTV_format/gunicorn_config.py
+++ py/media_manager_tools/LiveTV_format/gunicorn_config.py
@@ -4,11 +4,14 @@ worker_class = "gevent"  # 可以选择其他类型的 worker，例如 sync、ge
 loglevel = "info"  # 日志级别
 accesslog = "-"  # 将访问日志输出到标准输出
 errorlog = "-"  # 将错误日志输出到标准输出
-timeout = 120  # 超时时间
+# timeout = 120  # 超时时间
 # preload = True  # 启用预加载应用代码
 
 
 def on_starting(server):
     """只在主进程启动定时任务"""
+    import time
+
     from livetv_format.handle_cache import schedule_update_cached_file
     schedule_update_cached_file()
+    time.sleep(15)
